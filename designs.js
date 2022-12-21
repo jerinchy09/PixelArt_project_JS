c42e0dd9df8931f148dc2baf897cd05581ad16ca
Jerin <jerintasnimchy09@gmail.com>
2022-12-21 12:36:33 -0500

    updated JS File

---
+++ designs.js
@@ -0,0 +1,42 @@
+// Select color input
+// Select size input
+
+const sizepicker = document.querySelector("#sizePicker");
+const tbl = document.querySelector("#pixelCanvas");
+
+// When size is submitted by the user, call makeGrid()
+
+makeGrid(10, 10);
+
+sizepicker.addEventListener('submit', function (e) {
+    e.preventDefault();
+    while (tbl.hasChildNodes()) {
+        tbl.removeChild(tbl.firstChild);
+    }
+    let width = document.querySelector("#inputWidth").value;
+    let height = document.querySelector("#inputHeight").value;
+    makeGrid(width, height);
+
+});
+
+function makeGrid(width, height) {
+    for (let i = 0; i < width; i++) {
+        let row = tbl.insertRow(i);
+        for (let j = 0; j < height; j++) {
+            let cell = row.insertCell(j);
+            //set color
+            cell.style.backgroundColor = "white";
+            cell.addEventListener('click', function (e) {
+                e.preventDefault();
+                let color = document.querySelector("#colorPicker");
+                cell.style.backgroundColor = color.value;
+
+            });
+        }
+
+    }
+
+
+    // Your code goes here!
+
+}
