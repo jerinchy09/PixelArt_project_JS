5f7248ff911b7b08d1b231a0ff621e9ceafffc9a
Jerin <jerintasnimchy09@gmail.com>
2022-12-23 17:59:53 -0500

    Suggested Changes

--- designs.js
+++ designs.js
@@ -3,7 +3,8 @@
 
 const sizepicker = document.querySelector("#sizePicker");
 const tbl = document.querySelector("#pixelCanvas");
-
+let width = document.querySelector("#inputWidth");
+let height = document.querySelector("#inputHeight");
 // When size is submitted by the user, call makeGrid()
 
 makeGrid(5, 5);
@@ -13,16 +14,28 @@ sizepicker.addEventListener('submit', function (e) {
     while (tbl.hasChildNodes()) {
         tbl.removeChild(tbl.firstChild);
     }
-    let width = document.querySelector("#inputWidth").value;
-    let height = document.querySelector("#inputHeight").value;
-    makeGrid(width, height);
+    let w = width.value;
+    let h = height.value;
+    makeGrid(w, h);
 
 });
+/**
+  * @desc create a grid of squares 
+  * @param int $width - number of squares representing the width of the grid
+  * @param int $height - number of squares representing the height of the grid
+  * this function will have two parameters passed when this function will be called
+  * first loop will be used to set the required height
+  * the second loop will be used for corresponding cells till the required width  
+  * set the background color of the cell white
+  * event listener function will color the cells getting the color value from colorpicker 
+  * id in DOM for each click
+  *  
+*/
 
-function makeGrid(width, height) {
-    for (let i = 0; i < width; i++) {
+function makeGrid(w, h) {
+    for (let i = 0; i < h; i++) {
         let row = tbl.insertRow(i);
-        for (let j = 0; j < height; j++) {
+        for (let j = 0; j < w; j++) {
             let cell = row.insertCell(j);
             //set color
             cell.style.backgroundColor = "white";
@@ -30,13 +43,9 @@ function makeGrid(width, height) {
                 e.preventDefault();
                 let color = document.querySelector("#colorPicker");
                 cell.style.backgroundColor = color.value;
-
             });
         }
 
     }
-
-
-    // Your code goes here!
 
 }
